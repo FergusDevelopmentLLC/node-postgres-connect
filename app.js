3ba560132fb5eadb8a61097dbd8f76e069cfeec6
will.carter@fergusllc.com <will.carter@fergusllc.com>
2017-06-15 12:38:55 -0600

    simplified connect, added comments.

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-var pg = require('pg');
 var os = require('os');
 
-var port = 5432;
-
-console.log(os.hostname());
-console.log(os.type());
-
 var hostname = os.hostname();
+var type = os.type();
 
-if(hostname != "geodev")//node is being run on host, use the port forward
-  port = 15432;
+//display the host computer name
+console.log('os.hostname(): ' + os.hostname());
+//display the os type (Linux, Windows, etc.)
+console.log('os.type(): ' + os.type());
+console.log();
+
+//node is not being run locally.
+//port 15432 on the host is forwarded to 5432 on the vm
+var port = 5432;
+if (hostname !== "geodev") { port = 15432; }
 
 var conString = "postgres://geodevdb:admin123@127.0.01:" + port + "/geodevdb";
 
-//this initializes a connection pool
-//it will keep idle connections open for a (configurable) 30 seconds
-//and set a limit of 10 (also configurable)
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+var pg = require('pg');
+var client = new pg.Client(conString);
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1
-  });
+client.connect();
+
+var query = "SELECT * FROM weather;";
+console.log("SQL query on geodevdb: " + query);
+
+var query = client.query(query);
+
+query.on('row', function (row) {
+    console.log(row);
+});
+
+query.on('end', function () {
+    client.end();
 });
